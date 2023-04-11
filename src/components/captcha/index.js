9560a9922c9dd6290f1a0a2a7caa8df3e6f94480
mohammadghomizadeh <m.ghomizadeh96@gmail.com>
2023-04-11 13:13:12 +0430

    decoration-captcha-done

--- src/components/captcha/index.js
+++ src/components/captcha/index.js
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState, useRef } from 'react'
 import useCaptcha from 'use-offline-captcha'
-
+import Grid from '@mui/material/Grid';
 const Captcha = ({onActionSubmit,onActionValue}) => {
     const captchaRef = useRef()
     const [value, setValue] = useState()
@@ -8,10 +8,10 @@ const Captcha = ({onActionSubmit,onActionValue}) => {
         type: 'numeric', // "mixed"(default) | "numeric" | "alpha"
         length: 6, // 4 to 8 number. default is 5
         sensitive: false, // Case sensitivity. default is false
-        width: 300, // Canvas width. default is 200
-        height: 50, // Canvas height. default is 50
+        width: 220, // Canvas width. default is 200
+        height: 40, // Canvas height. default is 50
         fontColor: '#000',
-        background: 'rgba(255, 255, 255, .2)'
+        background: 'rgba(255, 255, 255, .2)',
     }
     const { gen, validate } = useCaptcha(captchaRef, userOpt)
 
@@ -28,12 +28,19 @@ const Captcha = ({onActionSubmit,onActionValue}) => {
 
     return (
         <>
-            <div ref={captchaRef} />
-            <input onChange={(e) => {
-              setValue(e.target.value);
-              onActionSubmit(validate(e.target.value))
-              onActionValue(e.target.value)
-            }} value={value} />
+          <Grid container spacing={2} columns={16}>
+            <Grid item md={8} xs={16} >
+                <input style={{height:'42px',width:'100%'}} onChange={(e) => {
+                  setValue(e.target.value);
+                  onActionSubmit(validate(e.target.value))
+                  onActionValue(e.target.value)
+                  
+                }} value={value} />
+            </Grid>
+            <Grid item md={8} xs={16}>
+                <div ref={captchaRef} />
+            </Grid>
+          </Grid>
             {/* <button onClick={handleValidate}>Validate</button> */}
             {/* <button onClick={handleRefresh}>Refresh</button> */}
         </>

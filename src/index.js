f40c234cf336113ac4e2fd7d47905d6a64db6b6a
Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>
2023-06-16 12:08:51 +0330

    lint

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ToastContainer, Slide } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '#c/functions/store';
-import App from '#c/App';
-import '#c/i18n';
-import {fetchTheme} from '#c/functions';
+
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
@@ -15,7 +12,14 @@ import '#c/assets/styles/global.css';
 import '#c/assets/styles/ltr.css';
 import '#c/assets/styles/rtl.css';
 import '@splidejs/react-splide/css/skyblue';
+
+import { store, persistor } from '#c/functions/store';
+import App from '#c/App';
+import '#c/i18n';
+import { fetchTheme } from '#c/functions';
+
 store.dispatch(fetchTheme());
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Provider store={store}>
